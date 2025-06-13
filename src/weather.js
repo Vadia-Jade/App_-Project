414e2742d5a9a9221ae6d0f67e38826d713b4762
Vadia-Jade <missnaturalfuzz4319@gmail.com>
2025-06-13 14:56:53 +0100

    added time and date

--- src/weather.js
+++ src/weather.js
@@ -1,9 +1,46 @@
 function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
-
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");  
+    let currentDateELement = document.querySelector("#current-date");
+    let currentDate = new Date();
+ 
+  
+    currentDateELement.innerHTML = formatDate(currentDate);
+    cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
 }
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+   
+    
+    let day = days [date.getDay()];
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+      } if (hours < 10) {
+        hours = `0${hours}`;
+      };
+  
+    return `${day} ${hours}:${minutes}`;
+  }
+
 function searchCity(city) {
  let apiKey = "034407t747af2dfd1c31bo02c7fc4156";
 
@@ -14,8 +51,7 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
    event.preventDefault();
    let searchInput = document.querySelector("#search-input");
-   let dataCityElement = document.querySelector("#data-city");
-   dataCityElement.innerHTML = searchInput.value;
+ 
    searchCity(searchInput.value);
 }
 let formSearchElement = document.querySelector("#form-search");
